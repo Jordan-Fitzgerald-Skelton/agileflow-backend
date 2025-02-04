2f08968a33a7a1bb4f397c4327561560a58878ef
Your Name <jordanfitzskelton45@gmail.com>
2025-02-04 14:06:41 +0000

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || 'http://localhost:4000',
+    origin: 'http://localhost:4000',
   },
 });
 
