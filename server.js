23f7bdb2f2365836472d5717c824fddbf46eee7b
Your Name <jordanfitzskelton45@gmail.com>
2025-03-21 11:38:52 +0000

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const nodemailer = require("nodemailer");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
+const io = new Server(server, { cors: { origin: "http://localhost:4000" } });
 
 const pool = new pg.Pool({
   user: process.env.DB_USER,
